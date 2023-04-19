16c7e049171a47cdd9f4cb43c9f16abf4959509e
najee sidahmed <najeekamil@msn.com>
2023-04-19 15:54:33 +0100

    fix api call issues

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,7 +1,7 @@
 import ai from "./ai.png";
 import folder from "./folder.png";
 import swatch from "./swatch.png";
-import download from "./download.png";
+import download from "./dl.png";
 
 import logoShirt from "./logo-tshirt.png";
 import stylishShirt from "./stylish-tshirt.png";
