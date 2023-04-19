cb003b0a26dce1e465c88e5dd26498eeeda8ef98
najee sidahmed <najeekamil@msn.com>
2023-04-19 23:06:14 +0100

    site title

--- client/src/pages/Customiser.jsx
+++ client/src/pages/Customiser.jsx
@@ -59,7 +59,7 @@ const Customiser = () => {
       // call backend to generate img
       setGeneratingImg(true);
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://threejs-ai-customs.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
