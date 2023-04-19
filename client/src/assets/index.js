efbc9acc0128ea6e11cbcfc549cd64bd0f2e2498
najee sidahmed <najeekamil@msn.com>
2023-04-19 15:19:24 +0100

    add all files

---
+++ client/src/assets/index.js
@@ -0,0 +1,9 @@
+import ai from "./ai.png";
+import folder from "./folder.png";
+import swatch from "./swatch.png";
+import download from "./download.png";
+
+import logoShirt from "./logo-tshirt.png";
+import stylishShirt from "./stylish-tshirt.png";
+
+export { ai, folder, swatch, download, logoShirt, stylishShirt };
